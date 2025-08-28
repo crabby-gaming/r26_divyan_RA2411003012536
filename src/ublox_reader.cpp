3302d9cf8af99d61e2843814395a167eea195431
crabby-gaming <divyandhawan@gmail.com>
2025-08-28 20:08:12 +0530

    update

--- src/ublox_reader.cpp
+++ src/ublox_reader.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 static int NAV_POSLLH(uint8_t *buffer, classId *gps) {
   memcpy(&gps->iTOW, buffer, 4);
-  memcpy(&gps->lon, buffer, 4);
-  memcpy(&gps->lat, buffer, 4);
+  memcpy(&gps->lon, buffer + 4, 4);
+  memcpy(&gps->lat, buffer + 8, 4);
   memcpy(&gps->height, buffer + 12, 4);
   memcpy(&gps->hMSL, buffer + 16, 4);
   memcpy(&gps->hAcc, buffer + 20, 4);
@@ -32,8 +32,8 @@ static vector<uint8_t> hexToBytes(const string &rawHex) {
 
 int decodeUBX(uint8_t *buffer, classId *gps) {
   // buffer points at class field
-  if (buffer[30] == 0x01 && buffer[32] == 0x02) { // Class = NAV, ID = POSLLH
-    return NAV_POSLLH(buffer + 4, gps);         // skip length
+  if (buffer[2] == 0x01 && buffer[3] == 0x02) { // Class = NAV, ID = POSLLH
+    return NAV_POSLLH(buffer + 6, gps);         // skip length
   }
   return 1;
 }
